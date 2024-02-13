289ef31bfffd5cefe9c419a768c87d3f48398037
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2024-02-13 08:35:45 -0500

    FirstCommit
    we create one example of one markup extension custom

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CrearExtensionDeMarcado
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
